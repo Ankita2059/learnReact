8e7279d3b0f363cb22195138c92931ba2bdc1b34
Ankita2059 <ankitakhatrichhetri@gmail.com>
2024-08-17 21:31:41 +0545

    React functional component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+const App = () => {
+  return (
+    <div>
+        <Header />
+        <h1>this is title</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur dolore sed ullam enim quia necessitatibus totam vel neque id ipsam.</p>
+        <Footer />    
+      </div>
+  )
+}
+
+export default App 
+
+
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <h2>Header</h2>
+            <p>this is a paragraph.</p>
+        </div>
+    )
+}
+
+export default Header;
